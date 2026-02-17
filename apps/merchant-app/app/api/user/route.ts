013ee2d2f32ec90235b692b86da47bfd7e50c25f
Luv Sharma <luvsharma105@gmail.com>
2026-02-18 01:53:16 +0530

    Added docker file

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,16 +1,11 @@
+"use server"
 import { NextResponse } from "next/server";
 import client from "@repo/db/client";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-// const adapter = new PrismaPg({
-//   connectionString: process.env.DATABASE_URL,
-// });
-
-// const client = new PrismaClient({ adapter });
-
-export const GET = async () => {
+export const POST = async () => {
   await client.merchant.create({
     data: {
       email: "asd",
